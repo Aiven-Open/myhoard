f18abcbaa3a90b86d1a6233841a0b6cc9bf77064
Cédric Thomas <cedric.thomas@scalingo.com>
2026-05-20 10:38:31 +0200

    fix: replace costly os.listdir with os.scandir

--- myhoard/restore_coordinator.py
+++ myhoard/restore_coordinator.py
@@ -503,8 +503,9 @@ class RestoreCoordinator(threading.Thread):
         if os.path.exists(self.mysql_data_directory):
             if not os.path.isdir(self.mysql_data_directory):
                 raise ValueError(f"MySQL data directory {self.mysql_data_directory!r} already exists and is not a directory")
-            if os.listdir(self.mysql_data_directory):
-                raise ValueError(f"MySQL data directory {self.mysql_data_directory!r} already exists and is not empty")
+            with os.scandir(self.mysql_data_directory) as entries:
+                if next(entries, None) is not None:
+                    raise ValueError(f"MySQL data directory {self.mysql_data_directory!r} already exists and is not empty")
 
         start_time = time.monotonic()
         restore_basebackup_info = self.state["basebackup_info"]
